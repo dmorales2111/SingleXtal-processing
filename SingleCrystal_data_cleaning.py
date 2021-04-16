6cad2c4b2674fe920afcd76c523c93355f4798e6
Daniel Morales <daniel.morales2111@gmail.com>
2021-04-16 13:04:08 -0400

    updated 4/16/21 1:00PM

--- SingleCrystal_data_cleaning.py
+++ SingleCrystal_data_cleaning.py
@@ -30,34 +30,37 @@ nuc = input('Enter which nucleus you want to work with: ')
 
 for fol in subdirs:
     compileMax(fol,nuc)
-    
-    
-    
-    
-    
-#helper functions    
+
+
+
+
+
+#helper functions
 def compileMax(folder,nucleus):
     os.chdir(direct + '\\' + folder)
     txtFiles = os_sorted([x for x in os.listdir() if '.txt' in x and nucleus in x])
-    
+
     if nuc == "Li7":
         # frequency axis is already in ppm for Li7, time to clean data
-        
+
         df_cleaned = pd.DataFrame(columns = ["Angle", "Real", 'ppm'])
-        df_cleaned["Angle"] = np.linspace(0,180,13)
-        
+
         for f in txtFiles:
             df = pd.read_csv(f, header = None).drop([0,1])
             df.columns = [df.iloc[0,0]]
             df = df.drop(2)
-            
+
+            #splits single column into three by tab delimiter of data
             df_split = df['Real\tImag\tppm'].apply(lambda x: pd.Series(x.split('\t')))
             df_split.columns = ['Real','Imag','ppm']
             df_split = df_split.drop(columns = ['Imag'])
             df_split = df_split.apply(lambda x: pd.to_numeric(x, errors = 'coerce'))
-            
+
             #find max intensity and peak position for selected spectra, and add it to existing table along with angle
             maxInt, peakPosition = df_split.loc[df2.idxmax()["Real"],:]
-
-    else:
+            angles = np.linspace(0,180,13)
+            for a in np.char.mod('%d',angles):
+                currentAngle = a if a in f
+            df_cleaned.append([float(currentAngle),maxInt,peakPosition])
+        else:
         #insert work for P31 here
