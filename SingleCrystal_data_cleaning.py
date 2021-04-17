3e5f6235dda5fc708e7acb5c4518c70ca77e805a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 12:24:20 -0400

    updated 4/17/21

--- SingleCrystal_data_cleaning.py
+++ SingleCrystal_data_cleaning.py
@@ -6,6 +6,7 @@ Created on Thu Apr 15 12:26:39 2021
 """
 
 import pandas as pd
+import numpy as np
 import os
 import tkinter as tk
 from tkinter import filedialog as fd
@@ -39,28 +40,39 @@ for fol in subdirs:
 def compileMax(folder,nucleus):
     os.chdir(direct + '\\' + folder)
     txtFiles = os_sorted([x for x in os.listdir() if '.txt' in x and nucleus in x])
+    df_cleaned = pd.DataFrame(columns = ["Angle", "Real", 'ppm'])
 
-    if nuc == "Li7":
-        # frequency axis is already in ppm for Li7, time to clean data
-
-        df_cleaned = pd.DataFrame(columns = ["Angle", "Real", 'ppm'])
-
-        for f in txtFiles:
-            df = pd.read_csv(f, header = None).drop([0,1])
-            df.columns = [df.iloc[0,0]]
-            df = df.drop(2)
-
+    for f in txtFiles:
+        df = pd.read_csv(f, header = None).drop([0,1])
+        df.columns = [df.iloc[0,0]]
+        df = df.drop(2)
+        if nuc == 'P31':
+            orig_ref_freq = float(input("Please enter the reference frequency in MHz: "))
+            obs_freq = float(input("Please enter transmitter frequency in MHz: "))
+            
             #splits single column into three by tab delimiter of data
-            df_split = df['Real\tImag\tppm'].apply(lambda x: pd.Series(x.split('\t')))
-            df_split.columns = ['Real','Imag','ppm']
-            df_split = df_split.drop(columns = ['Imag'])
+            df_split = df['Real\tImag\tHz'].apply(lambda x: pd.Series(x.split('\t')))
             df_split = df_split.apply(lambda x: pd.to_numeric(x, errors = 'coerce'))
-
-            #find max intensity and peak position for selected spectra, and add it to existing table along with angle
-            maxInt, peakPosition = df_split.loc[df2.idxmax()["Real"],:]
-            angles = np.linspace(0,180,13)
-            for a in np.char.mod('%d',angles):
-                currentAngle = a if a in f
-            df_cleaned.append([float(currentAngle),maxInt,peakPosition])
+            
+            #converts Hz to ppm using ref freq    
+            df_split.iloc[:,2] = (df_split.iloc[:,2] + 1e6*(obs_freq - orig_ref_freq))/orig_ref_freq 
+            
+        #otherwise, if nucleus is Li7, data is already in ppm, dont need to convert and can continue
         else:
+            df_split = df['Real\tImag\tppm'].apply(lambda x: pd.Series(x.split('\t')))
+            df_split = df_split.apply(lambda x: pd.to_numeric(x, errors = 'coerce'))
+       
+        df_split.columns = ['Real','Imag','ppm']
+        df_split = df_split.drop(columns = ['Imag'])
+
+        #find max intensity and peak position for selected spectra, and add it to existing table along with angle
+        maxInt, peakPosition = df_split.loc[df_split.idxmax()["Real"],:]
+        angles = np.linspace(0,180,13)
+            
+        currentAngle = [x for x in np.char.mod('%d',angles) if x in f]
+        new_row = pd.Series([float(currentAngle[0]), maxInt, peakPosition], index = df_cleaned.columns)
+        df_cleaned = df_cleaned.append(new_row, ignore_index = True)
+      
         #insert work for P31 here
+        
+        
